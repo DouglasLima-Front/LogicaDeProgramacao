f205210a7fa724f67473ec34aa0fdf978d2598b4
Douglas Lima <150302726+DouglasLima-Front@users.noreply.github.com>
2024-03-01 19:27:00 -0300

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,8 @@
+var numero1 = prompt("Digite o primeiro número:");
+var numero2 = prompt("Digite o segundo número:");
+var numero3 = prompt("Digite o terceiro número:");
+
+console.log("Os números digitados foram:");
+console.log("Número 1: " + numero1);
+console.log("Número 2: " + numero2);
+console.log("Número 3: " + numero3);
