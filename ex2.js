ae4729b51591effb5c1fcd833929796f3aa855cf
Douglas Lima <150302726+DouglasLima-Front@users.noreply.github.com>
2024-03-01 19:41:51 -0300

    Add files via upload

---
+++ ex2.js
@@ -0,0 +1,8 @@
+var numero1 = parseFloat(prompt("Digite o primeiro número:"));
+var numero2 = parseFloat(prompt("Digite o segundo número:"));
+var numero3 = parseFloat(prompt("Digite o terceiro número:"));
+
+var soma = numero1 + numero2 + numero3;
+var media = soma / 3;
+
+console.log("A média dos números digitados é: " + media);
